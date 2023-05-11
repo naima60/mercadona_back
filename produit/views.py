174c42269a8804193b4330ec8643724e8b4723b3
naima <cours.dev60@gmail.com>
2023-05-11 17:59:56 +0200

    modif settings

--- produit/views.py
+++ produit/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 
 def index(request):
     list_products = Produit.objects.filter(active='True').order_by('id')
-    paginator = Paginator(list_products, 15) # 15 articles par page
+    paginator = Paginator(list_products, 15)
     page = request.GET.get('page')
     products = paginator.get_page(page)
     return render(request, 'produit/index.html', context={'products': products})
